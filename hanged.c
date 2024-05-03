cf4ff4dcaabc9be8ef39ede7df5e609200b291bd
eugemigliaro <eugenio.migliaro@gmail.com>
2024-05-03 03:47:12 -0300

    terminé el ahorcado

--- hanged.c
+++ hanged.c
@@ -6,10 +6,13 @@
 #define MAX_ERRORS 6
 
 
-char print_hanged(int errors);
-char print_word(char word[]);
-char *set_answer(char *valid_words[], int amount);
-char *set_hanged_word(char *answer, char *guess);
+char print_hanged(int errors);//this works
+char print_word(char *word);//this works
+char *set_answer(char *valid_words[], int amount);//this works
+char *set_hanged_word(char *answer);//this works
+char check_guess(char *hanged_word, char *guess, char *answer, int *dashes);//i don't think this works
+char is_alpha(char *str);
+char play_game(char *hanged_word, char *guess, char *answer, int *errors, int *dashes);
 
 int main(){
 
@@ -17,10 +20,25 @@ int main(){
     srand(time(0));
 
     char *answer = set_answer(words, sizeof(words) / sizeof(words[0]));
+    int dashes = strlen(answer);
     int errors = 0;
-    printf("answer: %s\n", answer);
-    char *guess;
-    char *guesses = NULL;
+    char guess[100] = {0};
+
+    char *hanged_word = set_hanged_word(answer);
+    printf(hanged_word);
+    printf(answer);
+    print_word(hanged_word);
+    char won = 0;
+    while(won == 0 && errors < MAX_ERRORS){
+        won = play_game(hanged_word, guess, answer, &errors, &dashes);
+    }
+
+    print_hanged(errors);
+    print_word(hanged_word);
+
+    printf("%s, la palabra era %s\n", ((won) ? "Ganaste" : "Perdiste"), answer);
+
+    free(hanged_word);
 
     return 0;
 }
@@ -55,11 +73,13 @@ char print_hanged(int errors){
     return 0;
 }
 
-char print_word(char word[]){
-    for(int i = 0; !word[i]; i++){
+char print_word(char *word){
+    for(int i = 0; word[i]; i++){
         printf("%c ", word[i]);
     }
     printf("\n");
+
+    return 0;
 }
 
 char *set_answer(char *valid_words[], int amount){
@@ -67,12 +87,88 @@ char *set_answer(char *valid_words[], int amount){
 
     long random = (rand() + rand()) % amount;
     answer = valid_words[random];
+
+    return answer;
 }
 
-char *set_hanged_word(char *answer, char *guess){
+char *set_hanged_word(char *answer){
     int answer_length = strlen(answer);
     char *hanged_word = malloc(sizeof(char) * (answer_length + 1));
     for(int i = 0; i < answer_length; i++){
         hanged_word[i] = '_'; 
     }
+
+    hanged_word[answer_length] = '\0';
+
+    return hanged_word;
+}
+
+char check_guess(char *hanged_word, char *guess, char *answer, int *dashes){
+    char found = 0;
+    int guess_len = strlen(guess);
+
+    if(guess_len != 1){
+        if(strcmp(guess, answer) == 0){
+            found = 2;
+            strcpy(hanged_word, answer);
+        }
+        else{
+            found = 0;
+        }
+    }
+    else{
+        for(int i = 0; answer[i]; i++){
+            if(guess[0] == answer[i] && hanged_word[i] == '_'){
+                hanged_word[i] = guess[0];
+                found = 1;
+                (*dashes)--;
+            }
+        }
+
+        if(*dashes == 0){
+            found = 2;
+        }
+    }
+
+    return found;
+}
+
+char is_alpha(char *str){
+    char is_alpha = 1;
+
+    for(int i = 0; str[i] && is_alpha; i++){
+        if((str[i] < 'A' || str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z')){
+            is_alpha = 0;
+        }
+    }
+
+    return is_alpha;
+}
+
+char play_game(char *hanged_word, char *guess, char *answer, int *errors, int *dashes){
+    char won = 0;
+
+    print_hanged(*errors);
+    print_word(hanged_word);
+
+    int i = 0;
+
+    do{
+        printf("%sadivina una letra o la palabra entera: ", (i) ? ("dije una letra o palabra, ") : (""));
+        scanf(" %s", guess);
+        i++;
+    }while(!is_alpha(guess));
+
+    strupr(guess);
+
+    char check = check_guess(hanged_word, guess, answer, dashes);
+
+    if(check == 2){
+        won = 1;
+    }
+    else{
+        (*errors) += !check;
+    }
+
+    return won;
 }
