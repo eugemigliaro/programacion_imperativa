566eb604ee0a3b1f13a7f340f8da389aad2e6806
eugemigliaro <eugenio.migliaro@gmail.com>
2024-05-05 01:13:46 -0300

    listo el 1

--- taller 7/ej1.c
+++ taller 7/ej1.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <assert.h>
+#include <errno.h>
  
 #define OK 1
 #define NOOK !OK
@@ -10,6 +11,7 @@
  
 int obtenerAprobados(char * arrNombres[], int tam, char * aprobados[], const int notas[]);
 static void mostrarNombres(char * arrNombres[], size_t n);
+static void liberarAprobados(char * aprobados[], size_t n);
  
 int
 main(void) {
@@ -40,18 +42,38 @@ main(void) {
     assert(strcmp(aprobados3[1], "carlos") == 0);
  
     printf("\nOK!\n");
+
+    liberarAprobados(aprobados, tamApro);
+
     return 0;
 }
 
 int obtenerAprobados(char *arrNombres[], int tam, char *aprobados[], const int notas[]){
-    int ntam = 0;
+/*     int ntam = 0;
     for(int i = 0; i < tam; i++){
         if(notas[i] >= NOTA_APROBADO){
             aprobados[ntam++] = arrNombres[i];
         }
     }
 
-    return ntam;
+    return ntam; */ //this works, but now i want to try it copying the strings, not just the pointers to said strings
+
+    int ntam = 0, i, isOk = OK;
+
+    for(i = 0; i < tam && isOk; i++){
+        if(notas[i] >= NOTA_APROBADO){
+            aprobados[ntam] = malloc(strlen(arrNombres[i]) + 1);
+            if(aprobados[ntam] == NULL || errno == ENOMEM){
+                isOk = NOOK;
+            }
+            else{
+                strcpy(aprobados[ntam], arrNombres[i]);
+                ntam++;
+            }
+        }
+    }
+
+    return isOk ? ntam : 0;
 }
 
 static void mostrarNombres(char *arrNombres[], size_t n){
@@ -59,4 +81,10 @@ static void mostrarNombres(char *arrNombres[], size_t n){
         printf("%s ", arrNombres[i]);
     }
     printf("\n");
+}
+
+static void liberarAprobados(char *aprobados[], size_t n){
+    for(int i = 0; i < n; i++){
+        free(aprobados[i]);
+    }
 }
