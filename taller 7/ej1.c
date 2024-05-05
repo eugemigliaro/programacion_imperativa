c72b85b6db4f077f2297fb7f1b7b0e534b7fbb83
eugemigliaro <eugenio.migliaro@gmail.com>
2024-05-05 00:43:03 -0300

    agregué el test del taller 7

--- taller 7/ej1.c
+++ taller 7/ej1.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-
-/* Function declarations */
-int obtenerAprobados(char *arrNombres[], int tam, char *aprobados[], int notas[]);
-static void mostrarNombres(char *arrNombres[], size_t n);
-
-
-/* Main function */
-int main(void) {
-    /* Arrays of strings to store names and grades */
-    char *arrNombres[] = {"anita", "pepe", "fabi", "carlos", "tere"};
-    char *aprobados[5];
-    int notas[] = {3, 4, 8, 10, 1};
+#include <ctype.h>
+#include <assert.h>
+ 
+#define OK 1
+#define NOOK !OK
+#define NOTA_APROBADO 6
+ 
+int obtenerAprobados(char * arrNombres[], int tam, char * aprobados[], const int notas[]);
+static void mostrarNombres(char * arrNombres[], size_t n);
+ 
+int
+main(void) {
+    char * arrNombres[] = {"anita","pepe","fabi","carlos","tere"};
+    char * aprobados[5];
+    int notas[] = {3,4,8,10,1};
     int tam, tamApro;
-
-    /* Calculate the length of the names array */
-    tam = sizeof(arrNombres) / sizeof(char *);
-
-    /* Print the names */
+    tam = sizeof(arrNombres) / sizeof(char*);
     mostrarNombres(arrNombres, tam);
-
-    /* Get the approved students and their grades */
     tamApro = obtenerAprobados(arrNombres, tam, aprobados, notas);
-
-    /* Print the approved students */
     printf("Aprobados:\n");
     mostrarNombres(aprobados, tamApro);
-
-    /* Return 0 to indicate successful execution */
+ 
+    // Test del ejemplo
+    char * aprobados1[5];
+    assert(obtenerAprobados(arrNombres, 0, aprobados1, notas) == 0);
+    assert(obtenerAprobados(arrNombres, 1, aprobados1, notas) == 0);
+    assert(obtenerAprobados(arrNombres, 2, aprobados1, notas) == 0);
+    assert(obtenerAprobados(arrNombres, 3, aprobados1, notas) == 1);
+    assert(strcmp(aprobados1[0], "fabi") == 0);
+    char * aprobados2[5];
+    assert(obtenerAprobados(arrNombres, 4, aprobados2, notas) == 2);
+    assert(strcmp(aprobados2[0], "fabi") == 0);
+    assert(strcmp(aprobados2[1], "carlos") == 0);
+    char * aprobados3[5];
+    assert(obtenerAprobados(arrNombres, 5, aprobados3, notas) == 2);
+    assert(strcmp(aprobados3[0], "fabi") == 0);
+    assert(strcmp(aprobados3[1], "carlos") == 0);
+ 
+    printf("\nOK!\n");
     return 0;
 }
-
 
 int obtenerAprobados(char *arrNombres[], int tam, char *aprobados[], int notas[]){}
 
