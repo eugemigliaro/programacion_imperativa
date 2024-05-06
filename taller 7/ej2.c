357aa070a37449864f409ed55b8a1d48dd29f51a
eugemigliaro <eugenio.migliaro@gmail.com>
2024-05-05 22:25:47 -0300

    pete

--- taller 7/ej2.c
+++ taller 7/ej2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib/getnum.h"
   
 #define TOTAL_ITEMS 2
 #define MAX_LONG 10
@@ -22,4 +21,23 @@ main(void) {
     cargar(f, TOTAL_ITEMS);
     printf("%.2f", importe(f, TOTAL_ITEMS));
     return 0;
+}
+
+void cargar(tFactura fact, int cant){
+    for(int i = 0; i < cant; i++){
+        printf("Ingresá el código del item %d: ", i + 1);
+        scanf(" %d", &(fact[i].codigo));
+        printf("Ingresá la descripción del item %d: ", i + 1);
+        scanf(" %[^\n]%*c", fact[i].desc);
+        printf("Ingresá el precio del item %d: ", i + 1);
+        scanf(" %f", &(fact[i].precio));
+    }
+}
+
+float importe(tFactura fact, int cant){
+    float total = 0;
+    for(int i = 0; i < cant; i++){
+        total += fact[i].precio;
+    }
+    return total;
 }
